27648034969df3cf01b22d199cc7157da0c04c84
Vahan Asryan <vahan.asryan@avetiq.de>
2020-05-09 11:35:34 +0400

    Setup navigation initial version

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
 import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import Page from './components/layout/Page';
 import Main from './components/pages/Main';
+import Details from './components/pages/Details';
 
 import './App.css';
 
 function App() {
   return (
     <Page>
-      <Main />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <Main />
+          </Route>
+
+          <Route path="/details/:day">
+            <Details />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </Page>
   );
 }

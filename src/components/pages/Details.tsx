21b9eebd79c7e1c764e2bdb916d64fd712794f07
Vahan Asryan <vahan.asryan@avetiq.de>
2020-05-09 14:11:37 +0400

    Add List and Button to Details page and pass todos down to the list

--- src/components/pages/Details.tsx
+++ src/components/pages/Details.tsx
@@ -1,7 +1,30 @@
-import React from 'react';
+import React, {useMemo} from 'react';
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+
+import Button from '../ui/Button';
+import List from '../ui/List';
+
+import { getTodos } from '../../store/selectors/todos';
 
 const Details = () => {
-  return <div>Details page</div>;
+  const { day } = useParams();
+  const formatedDay = new Date(day).toDateString();
+
+  const data = useSelector(getTodos);
+  const todos = useMemo(() => {
+    return data.filter((todo) => todo.date === day);
+  }, [data, day]);
+
+  return (
+    <div>
+      <h2>Todo list due to {formatedDay}</h2>
+
+      <Button onClick={() => {}}>Add new todo</Button>
+
+      <List todos={todos} />
+    </div>
+  );
 };
 
 export default React.memo(Details);
